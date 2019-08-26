3f9890b42057a10135efb78b9148bfb634360c9e
litianfu <1035869369@qq.com>
2019-08-26 09:38:41 +0800

    "正在完成房屋的认证"

--- pages/user/myapprove/myapprove.js
+++ pages/user/myapprove/myapprove.js
@@ -5,62 +5,109 @@ Page({
    * 页面的初始数据
    */
   data: {
+    homeArray: [{
+        id: 0,
+        name: "小康小区",
+        isSelected: 1
+      },
+      {
+        id: 1,
+        name: "复兴小区",
+        isSelected: 0
+      },
+    ],
+  },
+  renzheng(){
+    wx.navigateTo({
+      url: '../approvehome/approvehome',
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {},
+    })
 
   },
+  unselected(e) { //房屋未选中时
+  console.log(e)
+    var that = this;
+    var arrays = this.data.homeArray;
+    wx.showModal({
+      title: '提示',
+      content: '您是否要切换房屋',
+      success(res) {
+        if (res.confirm) {
+          arrays.forEach(function (item, index) {
+            var str = "homeArray[" + index + "].isSelected"
+            if (e.currentTarget.dataset.id == item.id) {
+              that.setData({
+                [str]: 1,
+              })
+            } else {
+              that.setData({
+                [str]: 0,
+              })
+            }
+          })
+        } 
 
+      }
+    })
+    
+
+
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
