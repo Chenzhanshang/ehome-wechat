23425ff5f6a7e4e62605c96bf6a9abd14c620d0a
litianfu <1035869369@qq.com>
2019-08-27 09:41:07 +0800

    使用Uploader组件

--- pages/user/approvehome/building/homenum/approve/approve.js
+++ pages/user/approvehome/building/homenum/approve/approve.js
@@ -8,6 +8,13 @@ Page({
     xiaoquid: null,//小区号
     buildingid: null,//栋号
     buildNumId: null,//房号
+    files: [{
+      url: '',
+    }, {
+      loading: true
+    }, {
+      error: true
+    }]
   },
 
   /**
@@ -25,6 +32,10 @@ Page({
         xiaoquid: data.xiaoquid,
         buildNumId:data.buildNumId
       })
+    })
+    this.setData({
+      selectFile: this.selectFile.bind(this),
+      uplaodFile: this.uplaodFile.bind(this)
     })
 
   },
@@ -76,5 +87,50 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  chooseImage: function (e) {
+    var that = this;
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        console.log(res)
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        that.setData({
+          files: that.data.files.concat(res.tempFilePaths)
+        });
+      }
+    })
+    
+  },
+  previewImage: function (e) {
+    wx.previewImage({
+      current: e.currentTarget.id, // 当前显示图片的http链接
+      urls: this.data.files // 需要预览的图片http链接列表
+    })
+  },
+  selectFile(files) {
+    console.log('files', files)
+    // 返回false可以阻止某次文件上传
+  },
+  uplaodFile(files) {
+    console.log('upload files', files)
+    // 文件上传的函数，返回一个promise   
+    return new Promise((resolve, reject) => {
+      resolve({urls:""})//猜测为上传服务器地址
+      setTimeout(() => {
+        reject('some error')
+      }, 1000)
+    })
+  },
+  resolve(){
+
+  },
+  uploadError(e) {
+    console.log('upload error', e.detail)
+  },
+  uploadSuccess(e) {
+    console.log('upload success', e.detail)
+  },
+  
 })
